1ce58b0e97e54fbcc14e315e8ae38633fb0add5c
Fuad Hassan Siam <fuad.h.siam@gmail.com>
2025-12-13 02:03:17 +0600

    Synchronized all local changes, including filename fixes.

--- Simple_Price_Comparison.cpp
+++ Simple_Price_Comparison.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main ()
 {  
    string product;
-   double amazon, ebay, walmart;
+   long long amazon, ebay, walmart;
 
    cout << "What item do you want to buy: ";
    getline(cin, product);
@@ -19,8 +19,8 @@ int main ()
    cout << "Enter the price from walmart: $";
    cin >> walmart;
    
-   double cheapest = min({amazon, ebay, walmart});
-   double average = (amazon + ebay + walmart) / 3;
+   long long cheapest = min({amazon, ebay, walmart});
+   long long average = (amazon + ebay + walmart) / 3;
 
    cout << "For " << product << endl;
    cout << "Cheapest available price: $" << cheapest << endl;
@@ -28,6 +28,6 @@ int main ()
    cout << "Recomended budget for you: $" << average << endl;
    
 
-
+   return 0;
 
 }
